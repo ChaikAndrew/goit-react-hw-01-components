5efd0b52b27bcb25befff192467bb180a5ea1348
ChaikAndrew <101956196+ChaikAndrew@users.noreply.github.com>
2022-10-22 09:46:28 +0200

    add animation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,19 @@
 import user from 'components/data/user.json';
 import ProfileList from 'components/profile/ProfileList';
+
 export const App = () => {
   return (
     <div
       style={{
         padding: '50px',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
       }}
     >
+      {' '}
+      <div className="header_task">goit-react-hw-01-components</div>
       <ProfileList user={user} />
     </div>
   );
