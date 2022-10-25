c7fb8a7ede3e7c30b73de3b176c9f87da44e2341
ChaikAndrew <101956196+ChaikAndrew@users.noreply.github.com>
2022-10-25 14:03:25 +0200

    add 4task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import user from 'components/data/user.json';
 import friends from 'components/data/friends.json';
 import statistic from './data/statistic.json';
+import transactions from './data/transactions.json';
+
 import ProfileList from 'components/profile/ProfileList';
 import { FriendList } from './friendList/FriendList';
 import { Statistic } from './statistic/Statistic';
+import { Transaction } from './Transaction/Transaction';
 export const App = () => {
   return (
     <div
@@ -15,12 +18,12 @@ export const App = () => {
         alignItems: 'center',
       }}
     >
-      {' '}
       <div className="header_task">goit-react-hw-01-components</div>
       <ProfileList user={user} />
       <Statistic title="Upload stats" stats={statistic} />
       <Statistic stats={statistic} />
       <FriendList friends={friends} />
+      <Transaction transactions={transactions} />
     </div>
   );
 };
