b7aa151d1d065ef45cb24c79f7e03b6af892d3f2
ChaikAndrew <101956196+ChaikAndrew@users.noreply.github.com>
2022-10-27 12:43:36 +0200

    add icons for components
    add icons for online and offline users from react-icons

--- src/components/friendList/friendListItem/FriendListItem.jsx
+++ src/components/friendList/friendListItem/FriendListItem.jsx
@@ -1,10 +1,16 @@
 import styles from './FriendListItem.module.css';
+import { FcApprove, FcBadDecision } from 'react-icons/fc';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={styles.itemList}>
       <span className="status">
-        <div className={isOnline ? styles.online : styles.offline}></div>
+        {isOnline ? (
+          <FcApprove className={styles.onlineIcon} />
+        ) : (
+          <FcBadDecision className={styles.offlineIcon} />
+        )}
+        {/* <div className={isOnline ? styles.online : styles.offline}></div> */}
       </span>
 
       <img className={styles.avatar} src={avatar} alt={name} width="48" />
